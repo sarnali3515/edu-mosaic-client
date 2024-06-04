ad17b7d968c1707a9e7db03225a65f22510f07d8
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-06-05 01:29:46 +0600

    my-enroll class updated with get from db

--- src/pages/Dashboard/Student/MyEnrolledClass.jsx
+++ src/pages/Dashboard/Student/MyEnrolledClass.jsx
@@ -1,20 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
 
 
 const MyEnrolledClass = () => {
+    const axiosSecure = useAxiosSecure();
+    const { user } = useAuth();
+    // get
+    const { data: enrollClasses = [], isLoading, refetch } = useQuery({
+        queryKey: ['enroll-class', user?.email],
+        queryFn: async () => {
+            const { data } = await axiosSecure.get(`/enroll-class/${user?.email}`);
+            console.log(data);
+            return data;
+        }
+
+    });
     return (
         <div>
             <h2 className="text-3xl text-center font-bold mb-5">My Enrolled Classes</h2>
-            <div className="grid gap-8  md:grid-cols-3">
-                <div className="card w-full bg-base-100 shadow-xl">
-                    <figure><img className="h-64 w-full" src="https://i.ibb.co/HK5cBkZ/teach-online-2-1.jpg" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Graphics Design</h2>
-                        <p className="text-sm">Instructed by <span className="font-semibold">Maria Olivia</span></p>
-                        <div className="card-actions justify-end">
-                            <button className="btn bg-purple-600 text-white btn-sm">Continue</button>
+
+            <div className="grid gap-8  md:grid-cols-2">
+
+                {
+                    enrollClasses.map(enrollClass =>
+                        <div key={enrollClass._id} className="card w-full bg-base-100 shadow-xl">
+                            <figure><img className="h-64 w-full" src={enrollClass.photo} alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{enrollClass.title}</h2>
+                                <p className="text-sm">Instructed by <span className="font-semibold">{enrollClass.teacherName}</span></p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn bg-purple-600 text-white btn-sm">Continue</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    )
+                }
             </div>
         </div>
     );
